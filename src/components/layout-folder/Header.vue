536d569404d9f783ec77d9333e24bbee8b4a3981
Harzhyjnr <ayomideyusuf090@gmail.com>
2025-08-05 11:21:28 +0100

    Setting Up

---
+++ src/components/layout-folder/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="header-container">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" >Navbar</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" >Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/course" class="nav-link" >Course</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link" >About us</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/pricing" class="nav-link" >Pricing</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link" >Contact Us</router-link>
+        </li>
+        <li class="nav-item dropdown">
+          <router-link class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </router-link>
+          <ul class="dropdown-menu">
+            <li><router-link class="dropdown-item" >Action</router-link></li>
+            <li><router-link class="dropdown-item" >Another action</router-link></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><router-link class="dropdown-item" >Something else here</router-link></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '@/views/HomeView.vue'
+import CourseView from '@/views/CourseView.vue'
+import AboutView from '@/views/AboutView.vue'
+import PricingView from '@/views/PricingView.vue'
+import ConatctView from '@/views/ConatctView.vue'
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: '/', name: 'home', component: HomeView},
+    {path: '/course', name: 'course', component: CourseView,},
+    {path: '/about', name: 'about', component: AboutView},
+    {path: '/pricing', name: 'pricing', component: PricingView},
+    {path: '/contact', name: 'contact', component: ConatctView},
+
+  ],
 })
 
 export default router
